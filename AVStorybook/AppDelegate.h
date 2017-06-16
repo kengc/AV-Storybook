0de6f2b7e713cd6605c34670d534d0e584af45d1
kevin Cleathero <kengc@hotmail.com>
2017-06-16 13:57:52 -0700

    Project Setup

---
+++ AVStorybook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVStorybook
+//
+//  Created by Kevin Cleathero on 2017-06-16.
+//  Copyright © 2017 Kevin Cleathero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
